458699b9aef5aba18fb74d14b6b82be7e1418b5c
Renan Lima <techharpy@gmail.com>
2018-09-11 07:57:57 -0300

    Upload arquivos pt9

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 $(function(){
-	$('#mobile').click(function(){
+	$('#mobile').find('i').click(function(){
 		$('#bars').toggleClass('fa-times');
-		if($(this).find('ul').is(':visible')){
-			$(this).find('ul').hide();
+		if($('#mobile').find('ul.mobileM').is(':visible')){
+			$('#mobile').find('ul.mobileM').hide();
 			$('body').css('background','#fff')
 		}else{
-			$(this).find('ul').show();
+			$('#mobile').find('ul.mobileM').show();
 			$('body').css('background','#ccc')
+		}
+	})
+
+	$('.aWrapper,.aWrapperTroca').click(function(){
+		if($(this).parent().find('.submenu-mobile').is(':visible')){
+			$(this).parent().find('.submenu-mobile').hide();
+			$(this).parent().find('.aWrapperTroca').hide();
+			$(this).parent().find('.aWrapper').show();
+		}else{
+			$(this).parent().find('.submenu-mobile').show();
+			$(this).hide();
+			$(this).parent().find('.aWrapperTroca').css("display","flex");
 		}
 	})
 })
