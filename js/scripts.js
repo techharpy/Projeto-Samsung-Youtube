b2047205eb354d56978c0946ec2025895a91988c
Renan Lima <techharpy@gmail.com>
2018-09-17 18:27:34 -0300

    Upload arquivps pt11

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,24 @@ $(function(){
 			$(this).parent().find('.aWrapperTroca').css("display","flex");
 		}
 	})
+
+
+	$('.galaxy1,.galaxy2').click(function(e){
+		let video1 = 'https://www.youtube.com/embed/ziQL-aM6U6M'
+		let video2 = 'https://www.youtube.com/embed/aUC685bxlZ8'
+		let modal = $(this).parent().parent().parent().find('section.modal-video')
+		let verifica = $(this).attr("class").split(' ')[1];
+		modal.show()		
+		if(verifica == 'galaxy1'){
+			modal.find('.video').append(`<iframe src="${video1}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`)
+		}else if(verifica == 'galaxy2'){
+			modal.find('.video').append(`<iframe src="${video2}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`)
+		}		
+		$('.fechar').click(function(){
+			$('section.modal-video').hide()
+		})
+	})
+
+
+
 })
